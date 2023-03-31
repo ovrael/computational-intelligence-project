7118480561e62c8feafdbb23665645c7f7fa76f5
AnnaCichon <anna.cichon97@wp.pl>
2023-03-31 19:13:59 +0200

    Added goods

---
+++ Optimal_Route_Project/Assets/Scripts/Goods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [ExecuteInEditMode]
+    public enum GoodsType
+    {
+        Orange = 1, 
+        Tuna = 2, 
+        Uranium = 3,
+    }
+
+    [ExecuteInEditMode]
+    public class Goods
+    {
+        static readonly Random rng = new Random();
+
+        [SerializeField]
+        private int goodsType;
+        public int GoodsType
+        {
+            get { return goodsType; }
+            set { goodsType = value; }
+        }
+
+        [SerializeField]
+        private int weight = rng.Next(100,200);
+        private int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        private void RandomType()
+        {
+            Array values = Enum.GetValues(typeof(GoodsType));
+            GoodsType randomGoodsType = (GoodsType)values.GetValue(rng.Next(values.Length));
+        }
+
+
+
+
+    }
+
+
+
+}
