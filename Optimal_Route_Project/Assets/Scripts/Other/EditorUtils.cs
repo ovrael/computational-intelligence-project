9da5ce28bbaab6483022e172875d3f74be216a18
SonnyFixit <fikser@tlen.pl>
2023-04-20 21:45:35 +0200

    Added EditorUtil

---
+++ Optimal_Route_Project/Assets/Scripts/Other/EditorUtils.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Reflection;
+
+static class EditorUtils
+{
+    public static void ClearLog()
+    {
+        var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+    }
+}
